a85b8c62a523463e8214dff8e65ac6c94003cce9
OlekZima <zimenkoalex34@gmail.com>
2025-01-11 21:26:58 +0100

    Refactor anti_dede_bot.py for improved logging and configuration management

--- anti_dede_bot.py
+++ anti_dede_bot.py
@@ -1,12 +1,13 @@
-import datetime
+from datetime import datetime, timedelta
 import discord
 from discord.ext import tasks
 import logging
-import dotenv
+from dotenv import load_dotenv
 import os
 
-dotenv.load_dotenv()
-handler = logging.FileHandler(filename="discord_dede_delete.log", encoding="utf-8", mode="a")
+load_dotenv()
+logger = logging.getLogger("anti_dede_bot")
+handler = logging.FileHandler(filename="anti_dede_bot.log", encoding="utf-8", mode="a")
 
 
 class AntiDedeBot(discord.Client):
@@ -14,63 +15,71 @@ class AntiDedeBot(discord.Client):
         super().__init__(intents=intents, **options)
 
         self.messages: list[discord.Message] = []
-        self.dede_id = 207196996669407233
-        self.server_id = 1327588592301903954
-        self.channel_id = 1327588593362931755
+        self.dede_id = int(os.getenv("DEDE_ID"))
+        self.channel_id = int(os.getenv("CHANNEL_ID"))
 
     async def setup_hook(self) -> None:
         self.delete_messages.start()
 
     async def on_ready(self) -> None:
-        logging.info(f"Logged in as {self.user} (ID: {self.user.id})")
+        logger.info(f"Logged in as {self.user} (ID: {self.user.id})")
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=60)
     async def delete_messages(self) -> None:
         await self.fetch_messages()
         for message in self.messages:
-            if self.if_dede_message(message):
-                logging.info(f"Deleting message from {message.author} in {message.channel}")
+            if await self.is_dede_message(message):
+                logger.warning(f"Deleting message from {message.author} in {message.channel}")
                 await message.delete(delay=1)
 
     async def fetch_messages(self) -> None:
         await self.wait_until_ready()
         channel = self.get_channel(self.channel_id)
 
-        logging.log(logging.INFO, f"Checking dede messages in channel: {channel.name}")
+        logger.info(f"Checking dede messages in channel: {channel.name}")
         history = channel.history(
             limit=20,
-            after=datetime.datetime.now() - datetime.timedelta(minutes=5),
+            after=datetime.now() - timedelta(minutes=5),
             oldest_first=False,
         )
 
         self.messages = [
-            message async for message in history if await self.if_dede_message(message)
+            message async for message in history if await self.is_dede_message(message)
         ]
-        print(datetime.datetime.now() - datetime.timedelta(minutes=5))
 
-        logging.info(f"Found messages: {[message.content for message in self.messages]}")
+        logger.info(f"Found messages: {[message.content for message in self.messages]}")
 
-    async def if_dede_message(self, message: discord.Message) -> bool:
+    async def is_dede_message(self, message: discord.Message) -> bool:
         is_message_w_attachment = (
             len(message.attachments) >= 1 and message.author.id == self.dede_id
         )
+        logger.info(f"Message contains attachment(s): {is_message_w_attachment}.")
 
         if message.reference is None:
+            logger.info("Message does not contain a reference.")
             return is_message_w_attachment
+        try:
+            logger.info("Message contains a reference.")
+            original_message = await message.channel.fetch_message(message.reference.message_id)
+            is_reference_attachment = len(original_message.attachments) >= 1
+            logger.info("Reference contains attachment(s).")
+
+            return is_message_w_attachment or is_reference_attachment
+        except discord.NotFound:
+            logger.error("Original message not found.")
+        except discord.Forbidden:
+            logger.error("I have no permission to fetch message.")
+        except discord.HTTPException:
+            logger.error("An error occurred while fetching message.")
+        return False
 
 
-        original_message = await message.channel.fetch_message(message.reference.message_id)
-        is_original_attachment = len(original_message.attachments) >= 1
+if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.guilds = True
+    intents.guild_messages = True
+    intents.guilds = True
 
-        return is_message_w_attachment or is_original_attachment
-
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.guilds = True
-intents.guild_messages = True
-intents.guilds = True
-
-client = AntiDedeBot(intents=intents)
-client.run(os.getenv("DISCORD_TOKEN"), log_handler=handler)
+    client = AntiDedeBot(intents=intents)
+    client.run(os.getenv("DISCORD_TOKEN"), log_handler=handler)
